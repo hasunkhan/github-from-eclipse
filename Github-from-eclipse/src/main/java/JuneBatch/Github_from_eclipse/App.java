a9561210136b6475a089c14555874e67ef04f46b
Hasun Khan <hasunkhan26@gmail.com>
2024-02-24 17:33:49 -0500

    john's changes

--- Github-from-eclipse/src/main/java/JuneBatch/Github_from_eclipse/App.java
+++ Github-from-eclipse/src/main/java/JuneBatch/Github_from_eclipse/App.java
@@ -8,6 +8,7 @@ public class App
 {
     public static void main( String[] args )
     {
+         System.out.println( "Changes done by Akbar" );
         System.out.println( "Hello World!" + "First Change from Hassan" );
     }
 }
